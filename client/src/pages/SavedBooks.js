72fb1f488148baf819c6f420e38b504d77765f24
Melissa Lycan <melissamlycan@gmail.com>
2022-07-22 13:46:33 -0600

    pushing last code but struggling with signuo

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -28,35 +28,7 @@ const SavedBooks = () => {
     console.log(JSON.stringify(error));
   }
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error("something went wrong!");
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  //REFACTORED
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -65,24 +37,21 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      const { data } = await removeBook({
+        variables: { bookId: bookId },
+      });
       removeBookId(bookId);
+      console.log(data);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+  if (!userData.savedBooks.length) {
+    return <h2>You have not saved any books yet! Get searching!</h2>;
   }
 
   return (
