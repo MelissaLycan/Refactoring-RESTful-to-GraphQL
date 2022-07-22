97a504c9bcd7232aa6f2071186ed7e31e2296cb3
Melissa Lycan <melissamlycan@gmail.com>
2022-07-22 11:26:56 -0600

    updated resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,6 @@ const resolvers = {
     users: async () => {
       return User.find();
     },
-
     user: async (parent, { userId }) => {
       const userdata = await User.findOne({ _id: context.user._id });
       return userdata;
@@ -22,7 +21,7 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, { name, email, password }) => {
+    addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
 
@@ -46,34 +45,32 @@ const resolvers = {
     },
 
     // Add a third argument to the resolver to access data in our `context`
-    addSkill: async (parent, { userId, books }, context) => {
+    saveBook: async (parent, { input }, context) => {
       // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
       if (context.user) {
         return User.findOneAndUpdate(
-          { _id: userId },
+          { _id: context.user._id },
           {
-            $addToSet: { books: book },
+            $addToSet: { savedBooks: input },
           },
           {
             new: true,
-            runValidators: true,
           }
         );
       }
       // If user attempts to execute this mutation and isn't logged in, throw an error
-      throw new AuthenticationError("You need to be logged in!");
+      throw new AuthenticationError("Login to view this Info!");
     },
     // Set up mutation so a logged in user can only remove their profile and no one else's
 
     // Make it so a logged in user can only remove a skill from their own profile
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
+        return User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { books: bookId } },
-          { new: true }
+          { $pull: { savedBooks: bookId } },
+          { new: true, runValidators: true }
         );
-        return updatedUser;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
